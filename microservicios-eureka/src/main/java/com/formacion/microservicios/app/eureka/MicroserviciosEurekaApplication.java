deb66f562d8d4ba4bed9c8a5d81713921f57089f
jmtomeX <jmtome00@gmail.com>
2020-12-19 20:18:59 +0100

    Creado Microservicio Eureka-server

---
+++ microservicios-eureka/src/main/java/com/formacion/microservicios/app/eureka/MicroserviciosEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.formacion.microservicios.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// habilitar Eureka
+@EnableEurekaServer
+
+@SpringBootApplication
+public class MicroserviciosEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosEurekaApplication.class, args);
+	}
+
+}
