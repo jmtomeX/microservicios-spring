d533430b870a857dd1bba4c1a69bb18cbb5435fd
jmtomeX <jmtome00@gmail.com>
2020-12-25 20:40:12 +0100

    CAmbio en la base de datos a MySql

--- microservicio-usuarios/src/main/java/com/formacion/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicio-usuarios/src/main/java/com/formacion/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -5,21 +5,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.formacion.microservicios.app.usuarios.models.entity.Alumno;
 import com.formacion.microservicios.app.usuarios.services.AlumnoService;
 
-// marca la clase como controlador de tipo REST
 @RestController
 public class AlumnoController {
+	// inyectar el repositorio
 	@Autowired
 	private AlumnoService service;
 
--- microservicios-usuarios/src/main/java/com/formacion/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formacion.microservicios.app.usuarios.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.formacion.microservicios.app.usuarios.models.entity.Alumno;
-
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
-
-}
