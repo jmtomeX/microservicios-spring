b82391c6697224096da619449e1b660ed2b9d731
jmtomeX <jmtome00@gmail.com>
2021-01-12 16:58:39 +0100

    Entity Scan respuestas

--- microservicio-respuestas/src/main/java/com/formacion/microservicios/app/respuestas/MicroservicioRespuestaApplication.java
+++ microservicio-respuestas/src/main/java/com/formacion/microservicios/app/respuestas/MicroservicioRespuestaApplication.java
@@ -2,9 +2,15 @@ package com.formacion.microservicios.app.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @EnableEurekaClient
 @SpringBootApplication
+// para que JPA encuentre las clases examen, alumno, respuesta ya que tienen una relación
+@EntityScan({ "com.formacion.microservicios.app.respuestas.models.entity",
+		"com.formacion.microservicios.commons.alumnos.models.entity",
+		"com.formacion.microservicios.commons.examenes.models.entity" })
 public class MicroservicioRespuestaApplication {
 
 	public static void main(String[] args) {
