32e20e5171560f2aa2410f4f140d7e0f67699bc1
jmtomeX <jmtome00@gmail.com>
2021-01-15 18:35:59 +0100

    Creado microservicio gateaway para sustituirlo por zuul

---
+++ microservicio-gateway/src/test/java/com/formacion/microservicios/app/gateway/MicroservicioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.microservicios.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
