3d85b69a09505c4b748e54118884a338899d65d4
jmtomeX <jmtome00@gmail.com>
2021-03-03 18:06:18 +0100

    Eliminado collector y Empezando nuevas modificaones

--- microservicio-examenes/src/main/java/com/formacion/microservicios/app/examenes/controllers/ExamenController.java
+++ microservicio-examenes/src/main/java/com/formacion/microservicios/app/examenes/controllers/ExamenController.java
@@ -2,7 +2,6 @@ package com.formacion.microservicios.app.examenes.controllers;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
